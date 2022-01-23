978df77062407b81cbb93601ba1a9b20b04f7d70
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-01-23 21:04:29 +0530

    Example of Role Based Authorization

--- src/main/java/com/springsecurity/basics/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/basics/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.springsecurity.basics.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.AntPathMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -16,10 +18,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// also remember to create bean of PasswordEncoder otherwise we get: java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
 		auth.
 			inMemoryAuthentication().
-			withUser("username"). 
-			password("password").
-			roles("user");
+			withUser("user"). 
+			password("pass").
+			roles("USER").
+			and().
+			withUser("adminuser"). 
+			password("adminpass").
+			roles("ADMIN");
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Remember to go from most restrictive to least restrictive role
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasAnyRole("USER","ADMIN")
+			.antMatchers("/").permitAll()
+			.and().formLogin();
+		}
+	
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
--- src/main/java/com/springsecurity/basics/controller/SpringSecurityController.java
+++ src/main/java/com/springsecurity/basics/controller/SpringSecurityController.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringSecurityController {
 
+	// Available for everyone
 	@GetMapping("/")
 	public String helloWorld() {
 		return "<h1>Hello World</h1>";
+	}
+	
+	// Available only for USER and ADMIN roles
+	@GetMapping("/user")
+	public String helloUser() {
+		return "<h1>Hello User</h1>";
+	}
+	
+	// Available only for ADMIN roles
+	@GetMapping("/admin")
+	public String helloAdmin() {
+		return "<h1>Hello Admin</h1>";
 	}
 }
