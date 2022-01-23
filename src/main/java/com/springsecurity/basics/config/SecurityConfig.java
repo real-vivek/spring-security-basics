4dd176b4555613cb24c7c1ddb70ccf2db08a1a2f
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-01-23 21:43:23 +0530

    Example of JDBC Authentication using default schema and h2 database

--- src/main/java/com/springsecurity/basics/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/basics/config/SecurityConfig.java
@@ -1,43 +1,53 @@
 package com.springsecurity.basics.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.AntPathMatcher;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// Spring will autowire h2 database we have to only give spring-boot-starter-jdbc and h2 database dependency
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// Give username and password you want to authenticate with instead of user and pass
-		// also remember to create bean of PasswordEncoder otherwise we get: java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
-		auth.
-			inMemoryAuthentication().
-			withUser("user"). 
-			password("pass").
-			roles("USER").
-			and().
-			withUser("adminuser"). 
-			password("adminpass").
-			roles("ADMIN");
+		// We can create any number of users that we want using withUser
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.withDefaultSchema()
+		.withUser(
+				User.withUsername("user")
+				.password("pass")
+				.roles("USER")
+				)
+		.withUser(
+				User.withUsername("root")
+				.password("root")
+				.roles("ADMIN")
+				);
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Remember to go from most restrictive to least restrictive role
 		http.authorizeRequests()
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("USER","ADMIN")
-			.antMatchers("/").permitAll()
-			.and().formLogin();
-		}
-	
-	
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/").permitAll()
+		.and()
+		.formLogin();
+	}
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
